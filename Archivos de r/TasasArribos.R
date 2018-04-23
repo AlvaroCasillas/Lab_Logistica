fa4024e7022d917875d33b1d56011448a6d4f3e0
Lab. Logistica <LabLogistica@itam.mx>
2018-04-23 18:12:18 -0500

    simulación

--- Archivos de r/TasasArribos.R
+++ Archivos de r/TasasArribos.R
@@ -3,7 +3,21 @@ library(dplyr)
 library(ggplot2)
 
 
-lol <- read.csv("C:\\Users\\lablogistica\\Documents\\Laboratorio\\Lab_Logistica\\Archivos de excel\\tasasArribos_.csv")
+lol <- read.csv("C:\\Users\\lablogistica\\Documents\\Laboratorio\\Lab_Logistica\\Archivos de excel\\Estaciones_.csv")
 
 summary(lol)
-arribos <- matrix(nrow = 4067, ncol = 4067)
+
+#lol[1,]
+contador <- 0
+dim(lol)[1]
+
+for (i in 1:452) {
+  for (j in 1:(lol[i,2]-1)) {
+    lol <- rbind(lol, lol[i,])
+    #lol[dim(lol)[1],1] <- (i*1000)+j 
+  }
+}
+
+#write.csv(lol, file = "Datos_",row.names=FALSE, col.names = FALSE)
+
+lol_a <- lol[3701:4075,3:236]
